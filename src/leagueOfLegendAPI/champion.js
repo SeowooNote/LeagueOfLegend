6bdb17e26d0b0c76ff8f1b82a66b0129c09814d8
SeowooNote <dkr0210@naver.com>
2023-12-14 20:56:30 +0900

    Champion 작업중

--- src/leagueOfLegendAPI/champion.js
+++ src/leagueOfLegendAPI/champion.js
@@ -44,43 +44,45 @@ export default function Champion() {
     }, []);
 
     return (
-        <div className='flex flex-col items-center bg-lol-dark-blue'>
-            <div className='flex'>
-                <h1 className='text-lol-header-text-color'>리그오브레전드 챔피언 데이터</h1>
-            </div>
-            {championData ? (
-            <div className='flex flex-wrap w-4/5'>
-                {Object.values(championData.data).sort((a, b) => {
-                    return a.name.localeCompare(b.name);
-                }).map(champion => (
-                <div key={champion.id} onClick={() => onChampionHnadler(champion.id, champion.name, champion.blurb)} className='flex flex-col items-center transition hover:scale-125'>
-                    <img
-                    src={`https://ddragon.leagueoflegends.com/cdn/13.24.1/img/champion/${champion.id}.png`}
-                    alt={champion.name}
-                    />
-                    <h2 className='text-lol-header-text-color'>{champion.name}</h2>
+        <div className='flex justify-center items-center bg-lol-dark-blue'>
+            <div className='flex flex-col w-4/5 justify-center items-center '>
+                <div className='flex'>
+                    <h1 className='text-lol-header-text-color'>리그오브레전드 챔피언 데이터</h1>
                 </div>
-                ))}
-            </div>
-            ) : (
-            <p>Loading champion data...</p>
-            )}
-            {showPopup ? (
-                <div className='fixed w-4/5'>
-                    <div className='relative w-4/5 champion-box'>
-                        <div className='close' onClick={onCloseHandler}></div>
+                {championData ? (
+                <div className='flex flex-wrap justify-center'>
+                    {Object.values(championData.data).sort((a, b) => {
+                        return a.name.localeCompare(b.name);
+                    }).map(champion => (
+                    <div key={champion.id} onClick={() => onChampionHnadler(champion.id, champion.name, champion.blurb)} className='flex flex-col items-center transition hover:scale-125'>
                         <img
-                        src={`https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${championId}_0.jpg`}
-                        alt={championName}
-                        className='absolute w-full'
+                        src={`https://ddragon.leagueoflegends.com/cdn/13.24.1/img/champion/${champion.id}.png`}
+                        alt={champion.name}
                         />
-                        <div className='absolute text-7xl text-lol-text-color1 champion-name'>{championName}</div>
-                        <div className='absolute -1/4 text-3xl text-lol-text-color2 champion-description'>{championDescription}</div>
+                        <h2 className='text-lol-header-text-color'>{champion.name}</h2>
                     </div>
+                    ))}
                 </div>
-            ) : (
-                <></>
-            )}
+                ) : (
+                <p>Loading champion data...</p>
+                )}
+                {showPopup ? (
+                    <div className='w-4/5'>
+                        <div className='w-4/5 champion-box'>
+                            <div className='close' onClick={onCloseHandler}></div>
+                            <img
+                            src={`https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${championId}_0.jpg`}
+                            alt={championName}
+                            className='w-full'
+                            />
+                            <div className='text-7xl text-lol-text-color1 champion-name'>{championName}</div>
+                            <div className='text-3xl text-lol-text-color2 champion-description'>{championDescription}</div>
+                        </div>
+                    </div>
+                ) : (
+                    <></>
+                )}
+            </div>
         </div>
     )
 }
